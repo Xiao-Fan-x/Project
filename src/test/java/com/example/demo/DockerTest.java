5b7506cfe106da8c81ea6b752a0740728edbad4c
Xiao-Fan-x <yz96848715@126.com>
2022-05-09 16:42:48 +0800

    test docker-java

--- src/test/java/com/example/demo/DockerTest.java
+++ src/test/java/com/example/demo/DockerTest.java
@@ -2,19 +2,22 @@ package com.example.demo;
 
 
 import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.api.command.CreateContainerResponse;
-import com.github.dockerjava.api.command.LoadImageCmd;
-import com.github.dockerjava.api.model.ExposedPort;
-import com.github.dockerjava.api.model.Ports;
+import com.github.dockerjava.api.command.*;
+import com.github.dockerjava.api.model.*;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
-import com.github.dockerjava.core.DockerClientBuilder;
 import com.github.dockerjava.core.DockerClientConfig;
+import com.github.dockerjava.core.DockerClientImpl;
+import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
+import com.github.dockerjava.transport.DockerHttpClient;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.net.URISyntaxException;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import static com.github.dockerjava.api.model.HostConfig.newHostConfig;
@@ -23,42 +26,154 @@ import static com.github.dockerjava.api.model.HostConfig.newHostConfig;
 @SpringBootTest
 public class DockerTest {
 
+    String pyCode = "#!/usr/bin/python\n" +
+            "# Write Python 3 code in this online editor and run it.\n" +
+            "print(\"Hello, World!\");";
+
+    String javaCode = "public class HelloWorld {\n" +
+            "    public static void main(String []args) {\n" +
+            "       System.out.println(\"Hello World!\");\n" +
+            "    }\n" +
+            "}";
+
+    public static CreateContainerResponse createContainer(DockerClient client) throws URISyntaxException {
+
+        CreateContainerCmd containerCmd = client.createContainerCmd("nginx:latest")
+                //名字
+                .withName("tomcat")
+                //端口映射 内部80端口与外部81端口映射
+                .withHostConfig(new HostConfig().withPortBindings(new Ports(new ExposedPort(8080), Ports.Binding.bindPort(3355))))
+                //环境变量
+                .withEnv("key=value")
+                //挂载
+                .withVolumes(new Volume("/var/log"));
+
+        //创建
+        CreateContainerResponse response = containerCmd.exec();
+        System.out.println(response.getId());
+        return response;
+    }
 
     @Test
-    public void dockerClient() {
+    public void dockerClient() throws IOException {
 
         Properties properties = new Properties();
-//        properties.setProperty("registry.email", "info@baeldung.com");
-//        properties.setProperty("registry.password", "baeldung");
-//        properties.setProperty("registry.username", "baaldung");
-//        properties.setProperty("DOCKER_CERT_PATH", "/home/baeldung/.docker/certs");
-//        properties.setProperty("DOCKER_CONFIG", "/home/baeldung/.docker/");
-//        properties.setProperty("DOCKER_TLS_VERIFY", "1");
         properties.setProperty("DOCKER_HOST", "tcp://127.0.0.1:4243");
 
-        DefaultDockerClientConfig config
-                = DefaultDockerClientConfig.createDefaultConfigBuilder()
-                .withProperties(properties).build();
-        DockerClient dockerClient = DockerClientBuilder.getInstance(config).build();
+        DefaultDockerClientConfig config = DefaultDockerClientConfig
+                .createDefaultConfigBuilder()
+                .withProperties(properties)
+                .build();
 
-        dockerClient.startContainerCmd("python:3").exec();
-//        CreateContainerResponse container = dockerClient.createContainerCmd("openjdk:8-jdk-oracle")
-//                .withCmd("/bin/bash").exec();
+
+        DockerHttpClient httpClient = new ApacheDockerHttpClient.Builder()
+                .dockerHost(config.getDockerHost())
+                .maxConnections(100)
+                .connectionTimeout(Duration.ofSeconds(30))
+                .responseTimeout(Duration.ofSeconds(45))
+                .build();
+
+        DockerClient client = DockerClientImpl.getInstance(config, httpClient);
+
+
+        HostConfig hostConfig = HostConfig.newHostConfig();
+
+        List<Mount> list = new ArrayList<>();
+        Mount mount = new Mount();
+        mount.withSource("/temp/workspace");
+        mount.withTarget("/temp/workspace");
+
+        list.add(mount);
+        hostConfig.withMounts(list);
+
+        CreateContainerCmd containerCmd = client.createContainerCmd("tomcat:9.0")
+                //端口映射 内部80端口与外部81端口映射
+//                .withPortSpecs("3355")
+                //环境变量
+                //挂载
+                .withWorkingDir("/temp/workspace")
+                .withAttachStdout(true)
+                .withAttachStdout(true)
+//                .withBinds(Bind.parse("/var/local:var/local"))
+                .withHostConfig(HostConfig.newHostConfig())
+                .withTty(true);
+//                .withCmd("python", "demo.py");
+
+        CreateContainerResponse container = containerCmd.exec();
 //        client.startContainerCmd(container.getId()).exec();
-//        try {
-//            this.wait(5000);
-//            Thread.currentThread().notify();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        } finally {
-//            Thread.currentThread().notify();
-//        }
-//        client.stopContainerCmd(container.getId()).exec();
-//        client.removeContainerCmd(container.getId()).exec();
-//        return client;
-//        Info info = client.infoCmd().exec();
-//        System.out.println(JSONObject.toJSONString(info));
-//        return client;
+//        LogContainerCmd cmd = client.logContainerCmd(container.getId()).withStdOut(true);
+            client.crea
+        try {
+            client.execCreateCmd(container.getId()).withTty(true)
+                    .withAttachStdin(true).withAttachStdout(true).exec();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        InputStream stdin = client.execStartCmd(container.getId()).getStdin();
+
+        System.out.println(stdin);
+        stdin.close();
+//        System.out.println(cmd);
+//        adapter.onNext();
+    }
+
+
+    @Test
+    public void buildClient() throws IOException {
+
+        Properties properties = new Properties();
+        properties.setProperty("DOCKER_HOST", "tcp://127.0.0.1:4243");
+
+        DefaultDockerClientConfig config = DefaultDockerClientConfig
+                .createDefaultConfigBuilder()
+                .withProperties(properties)
+                .build();
+
+
+        DockerHttpClient httpClient = new ApacheDockerHttpClient.Builder()
+                .dockerHost(config.getDockerHost())
+                .maxConnections(100)
+                .connectionTimeout(Duration.ofSeconds(30))
+                .responseTimeout(Duration.ofSeconds(45))
+                .build();
+
+        DockerClient client = DockerClientImpl.getInstance(config, httpClient);
+
+
+        HostConfig hostConfig = HostConfig.newHostConfig();
+
+        File file = new File("/home/xiaofan/Documents/Project/Docker/Dockerfiles/Dockerfile");
+        String imageCmd = client.buildImageCmd()
+                .withDockerfile(file)
+                .withPull(true)
+                .withTarget("python:demo")
+                .exec(new BuildImageResultCallback())
+                .awaitImageId();
+
+
+        CreateContainerCmd containerCmd = client.createContainerCmd(imageCmd)
+                .withAttachStdout(true)
+                .withTty(true);
+//
+
+//        CreateContainerCmd containerCmd = client.createContainerCmd("tomcat:9.0")
+//                //名字
+//                .withName("tomcat")
+//                //端口映射 内部80端口与外部81端口映射
+//                .withHostConfig(new HostConfig().withPortBindings(new Ports(new ExposedPort(80), Ports.Binding.bindPort(81))))
+//                //环境变量
+//                //挂载
+//                .withVolumes(new Volume("/var/log"));
+
+
+        CreateContainerResponse container = containerCmd.exec();
+
+        client.startContainerCmd(container.getId()).exec();
+
+        LogContainerCmd cmd = client.logContainerCmd(container.getId()).withStdOut(true);
+
+        System.out.println(cmd);
+//        adapter.onNext();
     }
 
 
